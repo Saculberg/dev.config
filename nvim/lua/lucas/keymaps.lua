b9b7411aff3d44cff31d364c297d208e06e47d5e
LucasTietoevry <lucas.berg@tieto.com>
2026-06-12 12:27:01 +0200

    Lost this in an earlier commit

--- nvim/lua/lucas/keymaps.lua
+++ nvim/lua/lucas/keymaps.lua
@@ -20,3 +20,30 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Pane splitting (tmux-style)
+vim.keymap.set("n", "<leader>%", "<cmd>vsplit<CR>")
+vim.keymap.set("n", '<leader>"', "<cmd>split<CR>")
+
+-- Pane closing (tmux-style)
+vim.keymap.set("n", "<leader>x", "<cmd>close<CR>")
+
+-- Pane navigation (tmux-style)
+vim.keymap.set("n", "<leader><Left>", "<C-w>h")
+vim.keymap.set("n", "<leader><Down>", "<C-w>j")
+vim.keymap.set("n", "<leader><Up>", "<C-w>k")
+vim.keymap.set("n", "<leader><Right>", "<C-w>l")
+
+-- Tab/window management (tmux-style)
+vim.keymap.set("n", "<leader>c", "<cmd>tabnew<CR>")
+vim.keymap.set("n", "<leader>n", "<cmd>tabnext<CR>")
+vim.keymap.set("n", "<leader>&", "<cmd>tabclose<CR>")
+for i = 1, 9 do
+    vim.keymap.set("n", "<leader>" .. i, i .. "gt")
+end
+
+-- Pane resizing (tmux-style)
+vim.keymap.set("n", "<leader><M-Left>", "<C-w>2<")
+vim.keymap.set("n", "<leader><M-Down>", "<C-w>2-")
+vim.keymap.set("n", "<leader><M-Up>", "<C-w>2+")
+vim.keymap.set("n", "<leader><M-Right>", "<C-w>2>")
