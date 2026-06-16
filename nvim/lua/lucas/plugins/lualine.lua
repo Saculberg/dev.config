a04388d2a368a09bfa654d85798b604324f72bfa
Lucas <lucas@vraze.net>
2026-06-16 14:38:15 +0200

    Forgot to save

--- nvim/lua/lucas/plugins/lualine.lua
+++ nvim/lua/lucas/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 
     config = function()
         local colors = {
-            base    = '#191724',  -- rose-pine base
+            base    = '#191724', -- rose-pine base
             surface = '#1f1d2e',
             overlay = '#26233a',
             muted   = '#6e6a86',
@@ -19,37 +19,37 @@ return {
         }
 
         local bubbles_theme = {
-normal = {
-    a = { fg = colors.base, bg = colors.foam },
-    b = { fg = colors.text, bg = colors.surface },
-    c = { fg = colors.text },
-},
-    insert = {
-        a = { fg = colors.base, bg = colors.pine },
-        b = { fg = colors.text },
-        c = { fg = colors.text },
-    },
-    visual = {
-        a = { fg = colors.base, bg = colors.iris },
-        b = { fg = colors.text },
-        c = { fg = colors.text },
-    },
-    replace = {
-        a = { fg = colors.base, bg = colors.love },
-        b = { fg = colors.text },
-        c = { fg = colors.text },
-    },
-    command = {
-        a = { fg = colors.base, bg = colors.gold },
-        b = { fg = colors.text },
-        c = { fg = colors.text },
-    },
-    inactive = {
-        a = { fg = colors.muted },
-        b = { fg = colors.muted },
-        c = { fg = colors.muted },
-    },
-}
+            normal = {
+                a = { fg = colors.base, bg = colors.foam },
+                b = { fg = colors.text, bg = colors.surface },
+                c = { fg = colors.text },
+            },
+            insert = {
+                a = { fg = colors.base, bg = colors.pine },
+                b = { fg = colors.text },
+                c = { fg = colors.text },
+            },
+            visual = {
+                a = { fg = colors.base, bg = colors.iris },
+                b = { fg = colors.text },
+                c = { fg = colors.text },
+            },
+            replace = {
+                a = { fg = colors.base, bg = colors.love },
+                b = { fg = colors.text },
+                c = { fg = colors.text },
+            },
+            command = {
+                a = { fg = colors.base, bg = colors.gold },
+                b = { fg = colors.text },
+                c = { fg = colors.text },
+            },
+            inactive = {
+                a = { fg = colors.muted },
+                b = { fg = colors.muted },
+                c = { fg = colors.muted },
+            },
+        }
 
         require('lualine').setup {
             options = {
@@ -66,20 +66,19 @@ normal = {
                 lualine_x = {},
                 lualine_y = { 'filetype', 'progress' },
                 lualine_z = {
-                    { 'location', separator = { right = '' }, left_padding = 2 },    },
-                },
-                inactive_sections = {
-                    lualine_a = { 'filename' },
-                    lualine_b = {},
-                    lualine_c = {},
-                    lualine_x = {},
-                    lualine_y = {},
-                    lualine_z = { 'location' },
-                },
-                tabline = {},
-                extensions = {},
+                    { 'location', separator = { right = '' }, left_padding = 2 }, },
+            },
+            inactive_sections = {
+                lualine_a = { 'filename' },
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = { 'location' },
+            },
+            tabline = {},
+            extensions = {},
         }
         vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
-
     end,
 }
